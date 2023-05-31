118f8f71e14836c70717094ba237212be71c5159
stefansimereaa <stefanosimerea16@gmail.com>
2023-05-31 15:21:46 +0200

    exercise emails fixed

--- js/script2.js
+++ js/script2.js
@@ -5,7 +5,7 @@ console.log ('js ok')
 
 
 // Prendere elementi dal DOM
-const emailUser = document.getElementById('email').value;
+const emailUser = document.getElementById('email').value.toLowerCase();
 // Buttons 
 const button = document.getElementById(`control`)
 console.log(button);
@@ -15,7 +15,7 @@ console.log(button2);
 
 // Email Array
 
-const emails = ["email1@gmail.com", "email2@gmail.com", "email3@gmail.com", "email4@gmail.com", "email5@gmail.com"];
+const emails = ["email1@gmail.com", "email2@gmail.com", "email3@gmail.com", "email4@gmail.com", "email5@gmail.com" ];
 
 button.addEventListener('click', function(){
 
@@ -25,8 +25,10 @@ button.addEventListener('click', function(){
     result.classList.add(`d-block`, `d-flex`);
 
     //Controllo nel array
-    const emailPresent = false;
-    
+    let emailPresent = false;
+    let emailUser = document.getElementById('email').value.toLowerCase();
+
+
     for(let i = 0 ; i < emails.length; i++ ){
         if(emails[i] === emailUser){
             emailPresent = true;
@@ -35,7 +37,7 @@ button.addEventListener('click', function(){
     }
     
     if (emailPresent){
-        document.getElementById("results").innerText = "Benvenuto" + emailUser + "!";
+        document.getElementById("results").innerText += "Benvenuto" + " " +  emailUser + "!";
     } else {
         document.getElementById("results").innerText = "L'email " + emailUser + " non è presente.";
     }
